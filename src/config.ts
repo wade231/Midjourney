f9031a00f25ea8c02513a8e70be29cfa675476c1
wade231 <122469077+wade231@users.noreply.github.com>
2023-12-08 11:56:48 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ export default {
     clientId: "1109838882805125190",
     color: "#00ff00",
     replicateToken: "",
-    model: "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf" as any
+    model: "lucataco/realistic-vision-v5.1:2c8e954decbf70b7607a4414e5785ef9e4de4b8c51d50fb8b8b349160e0ef6bb" as any
 }
